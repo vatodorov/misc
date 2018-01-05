380ef0bfbe1b87cc54c1b30aed74c23719654029
valentin <vatodorov@gmail.com>
2018-01-05 12:29:29 -0500

    Update

---
+++ tests_.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 21 10:55:12 2017
+
+@author: Valentin.Todorov
+"""
+
+#!/bin/python
+
+import sys
+import os
+
+# Complete the function below.
+
+
+def  sumOfIntegers( arr):
+    return sum( arr)
+
+f = open(os.environ['OUTPUT_PATH'], 'w')
+    
+
+_arr_cnt = int(raw_input())
+_arr_i=0
+_arr = []
+while _arr_i < _arr_cnt:
+    _arr_item = int(raw_input());
+    _arr.append(_arr_item)
+    _arr_i+=1
+    
+
+res = sumOfIntegers(_arr);
+f.write(str(res) + "\n")
+
+f.close()
